9c840e67b01472e53d12aea97bec8f6d8dbdc745
Kaiky <kaikflamen@gmail.com>
2024-05-21 17:59:45 -0300

    Crud produto e categoria

---
+++ ecommerce/src/main/java/br/org/serratec/ecommerce/services/CategoriaService.java
@@ -0,0 +1,59 @@
+package br.org.serratec.ecommerce.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.org.serratec.ecommerce.entities.Categoria;
+import br.org.serratec.ecommerce.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
+	}
+
+	public Categoria findById(Integer id) {
+		return categoriaRepository.findById(id).get();
+	}
+
+	public Categoria save(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	public Categoria update(Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
+
+	public void delete(Categoria categoria) {
+		categoriaRepository.delete(categoria);
+	}
+
+	public void deleteById(Integer id) {
+		categoriaRepository.deleteById(id);
+	}
+
+	public long count() {
+		return categoriaRepository.count();
+	}
+
+	public boolean deleteById2(Integer id) {
+		if (categoriaRepository.existsById(id)) {
+			categoriaRepository.deleteById(id);
+			Categoria categoriaDeletado = categoriaRepository.findById(id).orElse(null);
+			if (categoriaDeletado == null) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+
+}

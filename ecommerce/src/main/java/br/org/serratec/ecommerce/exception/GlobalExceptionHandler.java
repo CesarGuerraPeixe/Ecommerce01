68875270f124ce538a3b0ea7b5c8b97d114473b2
Victor Siqueira <vg_siqueira@hotmail.com>
2024-05-22 17:53:11 -0300

    adicionado funções de status e exceções

---
+++ ecommerce/src/main/java/br/org/serratec/ecommerce/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package br.org.serratec.ecommerce.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
+		return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+	}
+
+}
